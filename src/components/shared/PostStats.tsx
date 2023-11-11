bcf0f3daf8cba74d4844870666977b876669e806
EricSousa02 <ericsousadeandrade@gmail.com>
2023-11-11 10:30:50 -0300

    loader when save

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -9,6 +9,7 @@ import {
   useDeleteSavedPost,
   useGetCurrentUser,
 } from "@/lib/react-query/queries";
+import { Loader } from ".";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -23,8 +24,8 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const [isSaved, setIsSaved] = useState(false);
 
   const { mutate: likePost } = useLikePost();
-  const { mutate: savePost } = useSavePost();
-  const { mutate: deleteSavePost } = useDeleteSavedPost();
+  const { mutate: savePost, isLoading: isSavingPost } = useSavePost();
+  const { mutate: deleteSavePost, isLoading: isDeletingPost } = useDeleteSavedPost();
 
   const { data: currentUser } = useGetCurrentUser();
 
@@ -76,22 +77,22 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
       className={`flex justify-between items-center z-20 ${containerStyles}`}>
       <div className="flex gap-2 mr-5">
         <img
-          src={`${
-            checkIsLiked(likes, userId)
+          src={`${checkIsLiked(likes, userId)
               ? "/assets/icons/liked.svg"
               : "/assets/icons/like.svg"
-          }`}
+            }`}
           alt="like"
           width={20}
           height={20}
           onClick={(e) => handleLikePost(e)}
           className="cursor-pointer"
         />
+
         <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
 
       <div className="flex gap-2">
-        <img
+        {isSavingPost || isDeletingPost ? <Loader /> : <img
           src={isSaved ? "/assets/icons/saved.svg" : "/assets/icons/save.svg"}
           alt="share"
           width={20}
@@ -99,6 +100,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           className="cursor-pointer"
           onClick={(e) => handleSavePost(e)}
         />
+        }
       </div>
     </div>
   );
