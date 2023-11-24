e1bb93b557c7dd8ab147ce4af63588c1ba8883da
EricSousa02 <ericsousadeandrade@gmail.com>
2023-11-24 09:24:10 -0300

    side bar animation

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,4 +1,5 @@
-import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import { INavLink } from "@/types";
 import { sidebarLinks, sidebarLinksPt } from "@/constants";
@@ -9,12 +10,22 @@ import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
 const LeftSidebar = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const { user, setUser, setIsAuthenticated, isLoading, currentLanguage, t, handleChangeLanguage } = useUserContext();
+  const {
+    user,
+    setUser,
+    setIsAuthenticated,
+    isLoading,
+    currentLanguage,
+    t,
+    handleChangeLanguage,
+  } = useUserContext();
 
   const { mutate: signOut } = useSignOutAccount();
 
+  const MotionLink = motion(Link);
+
   const handleSignOut = async (
-    e: React.MouseEvent<any, MouseEvent> 
+    e: React.MouseEvent<any, MouseEvent>
   ) => {
     e.preventDefault();
     signOut();
@@ -24,7 +35,13 @@ const LeftSidebar = () => {
   };
 
   return (
-    <nav className="leftsidebar">
+    <motion.nav
+      className="leftsidebar"
+      initial={{ x: -100, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: -100, opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="flex flex-col gap-10">
         <Link to="/" className="flex gap-1 items-center">
           <img
@@ -34,8 +51,9 @@ const LeftSidebar = () => {
             height={60}
             className=""
           />
-
-          <h1 className="font-bold tracking-wider text-xl">Write<span className="text-primary-500">Bomb</span></h1>
+          <h1 className="font-bold tracking-wider text-xl">
+            Write<span className="text-primary-500">Bomb</span>
+          </h1>
         </Link>
 
         {isLoading || !user.email ? (
@@ -51,91 +69,102 @@ const LeftSidebar = () => {
             />
             <div className="flex flex-col">
               <p className="body-bold truncate w-[150px]">{user.name}</p>
-              <p className="small-regular text-light-3 truncate">@{user.username}</p>
+              <p className="small-regular text-light-3 truncate">
+                @{user.username}
+              </p>
             </div>
           </Link>
         )}
 
         <ul className="flex flex-col gap-6">
-          {currentLanguage === "en" ?
-          (sidebarLinks.map((link: INavLink) => {
-            const isActive = pathname === link.route;
-
-            return (
-              <li
-                key={link.label}
-                className={`leftsidebar-link group ${
-                  isActive && "bg-primary-500"
-                }`}>
-                <NavLink
-                  to={link.route}
-                  className="flex gap-4 items-center p-4">
-                  <img
-                    src={link.imgURL}
-                    alt={link.label}
-                    className={`group-hover:invert-white ${
-                      isActive && "invert-white"
-                    }`}
-                  />
-                  {link.label}
-                </NavLink>
-              </li>
-            );
-          }))
-          :
-          (sidebarLinksPt.map((link: INavLink) => {
-            const isActive = pathname === link.route;
-
-            return (
-              <li
-                key={link.label}
-                className={`leftsidebar-link group ${isActive && "bg-primary-500"
-                  }`}>
-                <NavLink
-                  to={link.route}
-                  className="flex gap-4 items-center p-4">
-                  <img
-                    src={link.imgURL}
-                    alt={link.label}
-                    className={`group-hover:invert-white ${isActive && "invert-white"
+          {currentLanguage === "en"
+            ? sidebarLinks.map((link: INavLink, index: number) => (
+                <motion.li
+                  key={link.label}
+                  className={`leftsidebar-link group ${pathname === link.route &&
+                    "bg-primary-500"
+                  }`}
+                  initial={{ x: -50, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.1 * index, duration: 0.5 }}
+                >
+                  <MotionLink
+                    to={link.route}
+                    className="flex gap-4 items-center p-4"
+                  >
+                    <img
+                      src={link.imgURL}
+                      alt={link.label}
+                      className={`group-hover:invert-white ${
+                        pathname === link.route && "invert-white"
                       }`}
-                  />
-                  {link.label}
-                </NavLink>
-              </li>
-            );
-          }))
-        }
+                    />
+                    {link.label}
+                  </MotionLink>
+                </motion.li>
+              ))
+            : sidebarLinksPt.map((link: INavLink, index: number) => (
+                <motion.li
+                  key={link.label}
+                  className={`leftsidebar-link group ${pathname === link.route &&
+                    "bg-primary-500"
+                  }`}
+                  initial={{ x: -50, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.1 * index, duration: 0.5 }}
+                >
+                  <MotionLink
+                    to={link.route}
+                    className="flex gap-4 items-center p-4"
+                  >
+                    <img
+                      src={link.imgURL}
+                      alt={link.label}
+                      className={`group-hover:invert-white ${
+                        pathname === link.route && "invert-white"
+                      }`}
+                    />
+                    {link.label}
+                  </MotionLink>
+                </motion.li>
+              ))}
 
-          <li
-            className={`leftsidebar-link group`}>
-            <NavLink
+          <motion.li
+            className={`leftsidebar-link group`}
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.1 * sidebarLinks.length, duration: 0.5 }}
+          >
+            <MotionLink
               to="#"
               className="flex gap-4 items-center p-4"
-              onClick={handleChangeLanguage}>
-                
+              onClick={handleChangeLanguage}
+            >
               <img
                 src="../../../assets/icons/language.svg"
                 alt={t("Change language")}
                 className={`group-hover:invert-white `}
               />
               {t("Change language")}
-            </NavLink>
-          </li>
+            </MotionLink>
+          </motion.li>
         </ul>
       </div>
 
-      <div className='mt-12 flex flex-col gap-6 leftsidebar-link group p-4 hover:cursor-pointer'
-      onClick={(e) => handleSignOut(e)}>
-      <NavLink
-        to="#"
-        className="flex gap-4 items-center group-hover:invert-white "
-        onClick={(e) => handleSignOut(e)}>
-        <img src="/assets/icons/logout.svg" alt="logout" />
-        <p className="small-medium lg:base-medium">{t("Logout")}</p>
-      </NavLink>
+      <div
+        className="mt-12 flex flex-col gap-6 leftsidebar-link group p-4 hover:cursor-pointer"
+        onClick={(e) => handleSignOut(e)}
+      >
+        <MotionLink
+          to="#"
+          className="flex gap-4 items-center group-hover:invert-white "
+          onClick={(e) => handleSignOut(e)}
+        >
+          <img src="/assets/icons/logout.svg" alt="logout" />
+          <p className="small-medium lg:base-medium">{t("Logout")}</p>
+        </MotionLink>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
